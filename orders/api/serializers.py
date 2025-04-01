4f46c98c6321f755d85148e3042dde352b086c99
inara <inarashugaikina@gmail.com>
2025-04-01 17:21:40 +0500

    Override create/update methods of OrderSerializer

--- orders/api/serializers.py
+++ orders/api/serializers.py
@@ -19,7 +19,11 @@ class OrderDishSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     dishes = OrderDishSerializer(many=True)
     total_price = serializers.SerializerMethodField()
-    status = serializers.ChoiceField(choices=STATUSES)
+    status = serializers.ChoiceField(
+        choices=STATUSES,
+        error_messages={
+            'invalid_choice': f'Invalid status choice. Please choose from {[value[0] for value in STATUSES]}'}
+    )
 
     class Meta:
         model = Order
@@ -27,3 +31,26 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def get_total_price(self, obj):
         return sum([dish.price for dish in obj.dishes.all()])
+
+    def create(self, validated_data):
+        dishes = validated_data.pop('dishes')
+        order = Order.objects.create(**validated_data)
+
+        for dish in dishes:
+            OrderDish.objects.create(**dish, order=order)
+
+        return order
+
+    def update(self, instance, validated_data):
+        instance.table_number = validated_data.get('table_number', instance.table_number)
+        instance.status = validated_data.get('status', instance.status)
+        instance.total_price = validated_data.get('total_price', instance.total_price)
+
+        instance.save()
+
+        dishes_data = validated_data.pop('dishes', None)
+        if dishes_data is not None:
+            for dish_data in dishes_data:
+                OrderDish.objects.create(order=instance, **dish_data)
+
+        return instance
