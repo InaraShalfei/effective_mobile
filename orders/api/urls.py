128486405c88e0295faedd88c8ef40efa052a272
inara <inarashugaikina@gmail.com>
2025-04-02 18:48:14 +0500

    Exclude unnecessary endpoints from swagger documentation

--- orders/api/urls.py
+++ orders/api/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
+from djoser import views as djoser_views
 
 from api.views import OrderViewSet
 
 router = DefaultRouter()
 router.register('orders', OrderViewSet)
 
+user_registration = djoser_views.UserViewSet.as_view({
+    'post': 'create'
+})
+
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    re_path(r"^auth/jwt/create/?", jwt_views.TokenObtainPairView.as_view(), name="jwt-create"),
+    re_path(r"^auth/users/", user_registration, name="user-create"),
     path('', include(router.urls))
 ]
